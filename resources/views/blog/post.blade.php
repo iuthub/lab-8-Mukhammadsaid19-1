dc58c83122b67d0918dab0c2e95606720febd768
mukhammadsaid19 <m.mamasaidov@student.inha.uz>
2021-04-27 18:18:45 +0500

    completed

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">Learning Laravel</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Learning Laravel</h1>
+            <p>This blog post will get you right on track with Laravel!</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+    <hr>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,45 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+    'uses' => [\App\Http\Controllers\PostController::class, 'getIndex'],
+    'as' => 'blog.index'
+]);
+
+Route::get('post/{id}', [
+    'uses' => [\App\Http\Controllers\PostController::class, 'getPost',
+        'as' => 'blog.post']
+]);
+
+Route::get('/about', function () {
+    return view('other.about');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('', [
+        'uses' => [\App\Http\Controllers\PostController::class, 'getAdminIndex'],
+        'as' => 'admin.index'
+    ]);
+    Route::get('create', [
+        'uses' => [\App\Http\Controllers\PostController::class, 'getAdminCreate'],
+        'as' => 'admin.create'
+    ]);
+
+    Route::post('create', [
+        'uses' => [\App\Http\Controllers\PostController::class, 'postAdminCreate'],
+        'as' => 'admin.create'
+    ]);
+
+    Route::get('edit/{id}', [
+        'uses' => [\App\Http\Controllers\PostController::class, 'getAdminEdit'],
+        'as' => 'admin.edit'
+    ]);
+
+    Route::post('edit', [
+        'uses' => [\App\Http\Controllers\PostController::class, 'postAdminUpdate'],
+        'as' => 'admin.update'
+    ]);
+
+});
+
+
